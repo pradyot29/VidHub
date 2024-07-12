4ae88badc791d48fc9f8bd80f1b298047e0cc8b2
pradyot29 <sonipradyot@gmail.com>
2024-07-12 15:09:37 +0530

    adding new routes for user controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -5,10 +5,18 @@ import {
   registerUser,
   logOutUser,
   refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
 } from "../controllers/user.controller.js"; // aise import tb hi le skte h jb export default na ho
 
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { get } from "mongoose";
 //import { refreshAccessToken } from "../controllers/user.controller.js";
 const router = Router();
 
@@ -36,4 +44,11 @@ router.route("/login").post(loginUser);
 //secured routes
 router.route("/logout").post(verifyJWT, logOutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT,getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"), updateUserAvatar);
+router.route("/cover-image").patch(verifyJWT,upload.single("/coverImage"), updateUserCoverImage);
+router.route("/c/:userName").get(verifyJWT, getUserChannelProfile);
+router.route("/history").get(verifyJWT,getWatchHistory);
 export default router;
